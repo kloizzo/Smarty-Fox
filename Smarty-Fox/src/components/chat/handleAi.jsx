98a40ad9bd95e3cb61e148509f29972ddf334909
Kayla Loizzo <kaylaloizzo@gmail.com>
2023-08-04 11:56:23 -0400

    Changed prompt to have a kid friendly tone.

--- Smarty-Fox/src/components/chat/handleAi.jsx
+++ Smarty-Fox/src/components/chat/handleAi.jsx
@@ -9,7 +9,7 @@ const openai = new OpenAIApi(new Configuration({
 }));
 
 export async function handleAIReply(input) {
-  const prompt = `Pretend you are my tutor. I'm having trouble with ${input}. Do not give me the answer or tell me how to get the answer. Instead, explain the basic concept that will help me solve it on my own.`;
+  const prompt = `Pretend you are my tutor. I'm having trouble with ${input}. Do not give me the answer. Instead, explain the basic concept that will help me solve it on my own. Explain it to me like I am a kid. Sound friendly.`;
   const res = await openai.createChatCompletion({
     model: "gpt-3.5-turbo",
     max_tokens: 200,
